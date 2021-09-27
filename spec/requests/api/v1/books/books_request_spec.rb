3f2d9e3a880763f0a8ffe91445eaf2e156163b19
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-27 11:33:11 -0600

    Finish testing final endpoint

--- spec/requests/api/v1/books/books_request_spec.rb
+++ spec/requests/api/v1/books/books_request_spec.rb
@@ -10,6 +10,27 @@ describe 'books' do
 
       expect(response).to be_successful
       books = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(books[:id]).to eq(nil)
+      expect(books[:type]).to eq("books")
+
+      expect(books[:attributes]).to have_key(:destination)
+      expect(books[:attributes][:destination]).to eq(location)
+
+      expect(books[:attributes]).to have_key(:forecast)
+      expect(books[:attributes][:forecast]).to have_key(:summary)
+      expect(books[:attributes][:forecast]).to have_key(:temperature)
+
+      expect(books[:attributes]).to have_key(:total_books_found)
+
+      expect(books[:attributes]).to have_key(:books)
+      expect(books[:attributes][:books].count).to eq(quantity)
+
+      books[:attributes][:books].each do |book|
+        expect(book).to have_key(:isbn)
+        expect(book).to have_key(:title)
+        expect(book).to have_key(:publisher)
+      end
     end
   end
 end
