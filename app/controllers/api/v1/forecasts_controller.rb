5855a95b24bf4e0d3f1b11061d8dfb8c56cd1efb
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-26 22:35:49 -0600

    Render new serializer

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
     weather = ForecastFacade.retrieve_weather(params[:location])
+    render json: ForecastSerializer.new(weather)
   end
 end
