f4eb96ae47d468bf6a834d19063d3623f3cf8d25
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-28 17:43:37 -0600

    Add status 201

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = UserApiFacade.create_user_with_key(user_params)
     if user.valid?
-      render json: UsersSerializer.new(user)
+      render json: UsersSerializer.new(user), status: 201
     else
       render json: ErrorSerializer.error_json(user.errors.full_messages), status: 400
     end
