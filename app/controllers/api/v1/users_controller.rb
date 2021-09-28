4399a84deb1bd96682852f52197f3cb1c3142b1b
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-28 17:03:32 -0600

    Clean up code to use facade

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    if user_params[:password] == params[:password_confirmation]
-      user = User.create!(user_params)
-      user.api_keys.create!(token: SecureRandom.hex)
+    user = UserApiFacade.create_user_with_key(user_params)
+    if user.valid?
       render json: UsersSerializer.new(user)
+    else
+      render json: ErrorSerializer.error_json(user.errors.full_messages), status: 400
     end
   end
 
   private
 
   def user_params
-    params.permit(:email, :password)
+    params.permit(:email, :password, :password_confirmation)
   end
 end
