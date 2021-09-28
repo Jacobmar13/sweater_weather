0a7332e86ac3e595f2067ec3e0d851016a0759d7
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-28 17:44:00 -0600

    Add sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = UserApiFacade.authenticate(sessions_params)
+    if user.valid?
+      render json: UsersSerializer.new(user)
+    else
+      render json: ErrorSerializer.credentials_json, status: 400
+    end
+  end
+
+  private
+
+  def sessions_params
+    params.permit(:email, :password)
+  end
+end
