3e3c5cb59e079eca885cb979575fd87e88f13518
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-28 19:30:04 -0600

    Create validate session action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  def validate_session
+    if params[:api_key] && ApiKey.find_by(token: params[:api_key])
+    else
+      render json: ErrorSerializer.credentials_json, status: 400
+    end
+  end
 end
