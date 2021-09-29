b7a841b261db6286e8a1adc95d07fc2466469696
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-29 00:38:41 -0600

    Finish service testing

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe MapquestService, :vcr do
-  it 'returns a list latitude and longitude' do
+  it 'returns details on location given' do
     response = MapquestService.get_location('denver,co')
+
+    expect(response).to be_a(Hash)
+  end
+
+  it 'gives trip information between two destinations' do
+    trip_params = {
+      origin: "Denver,CO",
+      destination: "New York,NY"
+    }
+    response = MapquestService.get_travel_time(trip_params)
+
     expect(response).to be_a(Hash)
   end
 end
