76a4bc565ede88a04cc64e00a87edcf2ea81aca8
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-28 23:27:23 -0600

    Add attr_reader

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,8 +1,15 @@
 class RoadTrip
+  attr_reader :id,
+    :start_city,
+    :end_city,
+    :travel_time,
+    :weather_at_eta
+
   def initialize(trip_params, travel_time, forecast)
     @start_city = trip_params[:origin]
     @end_city = trip_params[:destination]
     @travel_time = Time.at(travel_time).utc.strftime "%Hh%Mm"
     @weather_at_eta = forecast
+    @id = nil
   end
 end
