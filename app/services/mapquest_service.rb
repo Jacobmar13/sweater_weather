cffce18d835bd9c9d42d3a3c6c25bdfd41293c71
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-28 23:22:52 -0600

    Add travel time get

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -11,5 +11,14 @@ class MapquestService
       end
       JSON.parse(response.body, symbolize_names: true)
     end
+
+    def get_travel_time(trip_params)
+      response = connection.get("/directions/v2/route") do |req|
+        req.params["key"] = ENV["MAPQUEST_ACCESS_KEY"]
+        req.params["from"] = trip_params[:origin]
+        req.params["to"] = trip_params[:destination]
+      end
+      JSON.parse(response.body, symbolize_names: true)
+    end
   end
 end
