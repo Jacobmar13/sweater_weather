4e6df7f2929e7e9e991620baeb7f6d4c3fef2b01
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-28 19:38:02 -0600

    Change error to unauthorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   def validate_session
     if params[:api_key] && ApiKey.find_by(token: params[:api_key])
     else
-      render json: ErrorSerializer.credentials_json, status: 400
+      render json: ErrorSerializer.unauthorized_json, status: 401
     end
   end
 end
