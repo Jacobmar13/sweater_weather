bf2c7381c02afc813ee56b1fbb4fdc91cfecdbc4
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-28 23:50:22 -0600

    Fix time display

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -8,8 +8,14 @@ class RoadTrip
   def initialize(trip_params, travel_time, forecast)
     @start_city = trip_params[:origin]
     @end_city = trip_params[:destination]
-    @travel_time = Time.at(travel_time).utc.strftime "%Hh%Mm"
+    @travel_time = time_calculator(travel_time)
     @weather_at_eta = forecast
     @id = nil
+  end
+
+  def time_calculator(time)
+    hour = (time / 3600).to_s.rjust(2,'0')
+    minute = (time / 60 % 60).to_s.rjust(2,'0')
+    "#{hour}h#{minute}m"
   end
 end
